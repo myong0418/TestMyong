067955bf5ff6ee1acfff46a481e60e8293c7a92a
myong <gyeong.mh@gmail.com>
2012-05-26 14:05:02 +0900

    Test myong

---
+++ TenVersion/src/com/smartschool/tenversion/SettingListActivity.java
@@ -0,0 +1,52 @@
+package com.smartschool.tenversion;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class SettingListActivity extends Activity  implements OnClickListener{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.setting);
+        
+//        //list btn
+//        Button safeBtn = (Button)findViewById(R.id.safe_btn);
+//        safeBtn.setOnClickListener(this);
+//        Button liveBtn = (Button)findViewById(R.id.live_btn);
+//        liveBtn.setOnClickListener(this);
+//        Button etcBtn = (Button)findViewById(R.id.etc_btn);
+//        etcBtn.setOnClickListener(this);
+//        
+//        //setting btn
+//        Button settingBtn = (Button)findViewById(R.id.setting_btn);
+//        settingBtn.setOnClickListener(this);
+        
+    }
+
+	public void onClick(View v) {
+//		switch(v.getId()) {
+//		
+//		case R.id.safe_btn:
+//			
+//			break;
+//			
+//		case R.id.live_btn:
+//			
+//			break;
+//			
+//		case R.id.etc_btn:
+//			
+//			break;
+//			
+//		case R.id.setting_btn:
+//			
+//			break;
+//			
+//		}
+		
+	}
+}
