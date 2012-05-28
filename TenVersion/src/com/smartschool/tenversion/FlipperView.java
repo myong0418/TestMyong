29f608817440a353a3dad6ba17394bb36674107d
myong <gyeong.mh@gmail.com>
2012-05-29 04:31:38 +0900

    add comment

---
+++ TenVersion/src/com/smartschool/tenversion/FlipperView.java
@@ -0,0 +1,12 @@
+package com.smartschool.tenversion;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class FlipperView extends Activity{
+	  @Override
+	    public void onCreate(Bundle savedInstanceState) {
+	        super.onCreate(savedInstanceState);
+	        setContentView(R.layout.flipperview);
+	  }
+}
